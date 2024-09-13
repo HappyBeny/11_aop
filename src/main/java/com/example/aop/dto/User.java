fb503851060b0e9a2e186095e79de09cd4c67c44
Beny Bang <qkdehdgur3@gmail.com>
2024-09-13 12:22:27 +0900

    8. aop 완료

---
+++ src/main/java/com/example/aop/dto/User.java
@@ -0,0 +1,41 @@
+package com.example.aop.dto;
+
+public class User {
+
+    private String id;
+    private String pw;
+    private String Email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", pw='" + pw + '\'' +
+                ", Email='" + Email + '\'' +
+                '}';
+    }
+}
